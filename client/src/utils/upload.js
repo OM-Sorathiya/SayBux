827a1edcebd91bdd5b02883eb14c57a2fd27f997
OM-Sorathiya <123730426+OM-Sorathiya@users.noreply.github.com>
2023-07-21 16:21:56 +0530

    Key changes

--- client/src/utils/upload.js
+++ client/src/utils/upload.js
@@ -6,8 +6,7 @@ const upload = async (file) => {
   data.append("upload_preset", "SayBux");
 
   try {
-    const res = await axios.post("https://api.cloudinary.com/v1_1/dgew5ktff/image/upload", data);
-    // import.meta.env.VITE_UPLOAD_LINK
+    const res = await axios.post(import.meta.env.VITE_UPLOAD_LINK, data);
     const { url } = res.data;
     return url;
   } catch (err) {
